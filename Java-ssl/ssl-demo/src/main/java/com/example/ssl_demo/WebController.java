26f4d02777c00b3afbc6eee327ac0ee68b5a2e52
alllna <alinazbaeva@gmail.com>
2025-12-08 00:39:19 +0300

    Update WebController.java

--- Java-ssl/ssl-demo/src/main/java/com/example/ssl_demo/WebController.java
+++ Java-ssl/ssl-demo/src/main/java/com/example/ssl_demo/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
