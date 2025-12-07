0da63b994afae6305e4cb2299f213b7b07c46961
alllna <alinazbaeva@gmail.com>
2025-12-07 23:18:08 +0300

    Add files via upload

---
+++ Java-ssl/ssl-demo/src/test/java/com/example/ssl_demo/WebController.java
@@ -0,0 +1,23 @@
+package com.example.ssl_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    
+    @GetMapping("/")
+    public String home() {
+        return "Главная страница работает!";
+    }
+    
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+    
+    @GetMapping("/ssl-test")
+    public String sslTest() {
+        return "SSL тест работает!";
+    }
+}
